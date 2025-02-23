b49fda9c0ad5a8582957cb91c3c4dedeafd22159
Myroslav Toloshnyi <dfgshte@gmail.com>
2025-02-23 22:39:21 +0200

    Experiment with loading content

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
+
+$env = parse_ini_file(__DIR__ . '/.env');
+foreach ($env as $key => $value) {
+    $_ENV[$key] = $value;
+}
+
+$environment = $_ENV['APP_ENV'] ?: 'production';
+
+// Для локальной разработки не нужно редиректить на 404 в случае отсутствия файла
+if ($environment === 'development') {
+    echo "Loaded on env: $environment<br />";
+    $devFilePath = __DIR__ . '/index.html';
+    if (file_exists($devFilePath)) {
+        $content = file_get_contents($devFilePath);
+        echo $content;
+        exit;
+    }
+}
+
 // Получаем User-Agent из заголовков запроса
 $userAgent = $_SERVER['HTTP_USER_AGENT'];
 
